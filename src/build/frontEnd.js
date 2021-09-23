53b9b9787c708d3942673f8b7f42d1853225660d
Neil Shah <neilshahlimited@hotmail.com>
2021-09-23 12:02:37 +0100

    created nft auction address scripts

--- src/build/frontEnd.js
+++ src/build/frontEnd.js
@@ -1,15 +1,21 @@
-console.log(JSON.stringify(Minima))
 
-Minima.init((res) => {
-    if (res.event === 'connected') {
-        console.log('Minima successfully loaded')
-    }
-})
+
+// console.log(JSON.stringify(Minima))
+
+// Minima.init((res) => {
+//     if (res.event === 'connected') {
+//         console.log('Minima successfully loaded')
+//     }
+// })
 
 
 // Get all the NFTs that are in my wallet
 function getAllMyNFTs() {
-
+    const command = 'balance'
+    cmd(command).then((res) => {
+        console.log('BALANCE')
+        console.log(JSON.stringify(res))
+    })
 }
 
 
@@ -20,10 +26,72 @@ function transferToNeil() {
 // "send            ": "[amount] [address] (tokenid|tokenid statevars) - Send Minima or Tokens to a certain address.",
 function transfer(amount, coin, toWalletAddress) {
     const command = `send ${amount} ${toWalletAddress} ${coin}`
-    Minima.cmd(command, (res) => {
+    cmd(command).then((res) => {
         console.log('TRANSFER')
         console.log(JSON.stringify(res))
     })
 }
 
+
+let AUCTION_CONTRACT_ADDRESS = ''
+let MY_NFT_TOKEN_ID = 'blah bl;ah'
+
+// script to register NFT at auction address
+
+// create contract
+// LET key = PREVSTATE (23)
+// RETURN SIGNEDBY ( key )
+// part of extra script
+function createContract() {
+    return new Promise((resolve, reject) => {
+        auctionAddressString = `extrascript "LET key = PREVSTATE ( 23 ) RETURN SIGNEDBY ( key )"`
+        Minima.cmd(auctionAddressString, (res) => {
+            console.log(res)
+            let hex = res.response.hexaddress
+            resolve(hex)
+        })
+    })
+    
+}
+
+
+// send NFT to contract
+function sendNFT(hexAddress) {
+    return new Promise((resolve, reject) => {
+        let command = `send 1 ${hexAddress} ${MY_NFT_TOKEN_ID}`
+        Minima.cmd(command, (res) => {
+            console.log(res)
+            let coinid = res.txpow.body.inputs[0].coinid
+            resolve(coinid)
+        })
+    })
+    
+}
+
+
+
+// check the nft is still spendable by me
+// *********** this cancels the auction *********
+function checkNFTSpendable(nftCoinId, selfAddress, nftTokenId, pubKeyUsedInScript) {
+    let command = `txncreate 10;
+        txninput 10 ${nftCoinId};
+        txnoutput 10 1 ${selfAddress} ${nftTokenId};
+        txnsign 10 ${pubKeyUsedInScript};
+        txnpost 10;
+        txndelete 10`
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
 transferToNeil()
+getAllMyNFTs()
