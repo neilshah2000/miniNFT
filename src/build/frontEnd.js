cc83024efa9748adbfb2d8753299de6ef4910190
Elias <eliasnemr@gmail.com>
2021-09-27 18:00:56 +0300

    Bidding

--- src/build/frontEnd.js
+++ src/build/frontEnd.js
@@ -5,6 +5,8 @@
 // tokenid: "0x58CA2E50302556E91E25878934726D87AE1FB51BAD66D70E62B68ED6328766D3DD3560F98AFA6B8CDABA4BAD9746C6FCDDAF600BAB717F0B84558FE7C33CE3D5"
 // coinid: "0xE19F61D56CE0D260A7C3744FA47E24194DFD137B560170BA712DA2A3ECDFCA7CA589E6344A27D1E26B165E1659F2485FBBB359EF49A7E6D273E7DD2513BF969F"
 
+const { Minima } = require("./minima");
+
 // sendNFT('0xFE03136653EA81F993A1C55CC24A00E788AEE0C5', '0x58CA2E50302556E91E25878934726D87AE1FB51BAD66D70E62B68ED6328766D3DD3560F98AFA6B8CDABA4BAD9746C6FCDDAF600BAB717F0B84558FE7C33CE3D5', '0x9E2C4D16466D57A45D3EF9B42A84A424B7B175DF')
 
 
@@ -30,6 +32,7 @@ var mPUBLICKEY = '';
 var uNFTCOINID = '';
 var mADDRESS = '';
 var sADDRESS = '';
+var aTOKENIWOULDWANT = '';
 
 var TOKENID = ''; // manual
 var SCALE = 0; // manual
@@ -128,6 +131,11 @@ function checkNFTSpendable(nftCoinId, selfAddress, nftTokenId, pubKeyUsedInScrip
     Minima.cmd(command, console.log)
 }
 
+// List all auctions
+function listAuctions() {
+
+}
+
 
 async function setAll() {
     
@@ -172,4 +180,79 @@ async function setAll() {
     // checkNFTSpendable(oNFTCOINID, mADDRESS, TOKENID, mPUBLICKEY, SCALE);
 
 
+}
+
+/**
+ * 
+ * LET bidderpubkey  = PREVSTATE(0)
+    LET bidderaddress = PREVSTATE(1)
+    LET token = PREVSTATE(2)
+
+IF SIGNEDBY ( bidderpubkey ) AND @BLKDIFF GT 100
+        THEN RETURN TRUE 
+ENDIF
+
+RETURN VERIFYOUT (@INPUT 1 bidderaddress token )
+ */
+
+// Bidder
+async function bidContract() {
+    const bidHexAddress = await createBidContract();
+    
+    const anAddress = await newAddress()
+    const aKey = await newKey()
+
+
+    createBidTransaction(2, bidHexAddress, anAddress, aKey, aTOKENIWOULDWANT);
+    
+}
+
+function createBidTransaction(amount, scriptAddress, myAddress, myPubKey, tokenIdIWant) {
+    const sendTransaction = `send ${amount} ${scriptAddress} 0:${myPubKey} 1:${myAddress} 2:${tokenIdIWant}`
+    Minima.cmd(sendTransaction, console.log)
+}
+
+// The Host accepts bid of bidder
+function acceptBid(inputCoinId1, inputCoinId2, outputAmount1, outputAmount2, tokenIdThatBidderWants, hostPubKey, hostAddress, bidderAddress) {
+    // construct manual transaction
+    const minimaTOKEN = '0x00';
+    let minimaAmount = 1 / Math.pow(10, scale)
+    // create 11
+    // minima coin in input 0
+    // token coin in input 1
+    // output coin of minima 0  
+    // output coin of token 1
+
+    let command = `txncreate 11;
+        txninput 11 ${inputCoinId1} 0;
+        txninput 11 ${inputCoinId2} 1;
+        txnoutput 11 ${outputAmount1} ${hostAddress} ${tokenIdThatBidderWants} 0;
+        txnoutput 11 ${outputAmount2} ${bidderAddress} ${minimaTOKEN} 1;
+        txnpost 11;
+        txndelete 11`
+    Minima.cmd(command, console.log)
+
+}
+// Bidder cancels his bid if blkDiff gte 100 and signed by bidder
+function cancelMyBid() {
+
+}
+
+function createBidContract() {
+    return new Promise((resolve, reject) => {
+        bidScript = `extrascript "LET bidderpubkey  = PREVSTATE(0)
+                                    LET bidderaddress = PREVSTATE(1)
+                                    LET token = PREVSTATE(2)
+                                
+                                    IF SIGNEDBY ( bidderpubkey ) AND @BLKDIFF GT 100
+                                            THEN RETURN TRUE 
+                                    ENDIF
+                                    
+                                    RETURN VERIFYOUT (@INPUT 1 bidderaddress token ) "`;
+        Minima.cmd(bidScript, (res) => {
+            console.log(res)
+            let hex = res.response.address.hexaddress
+            resolve(hex)
+        })
+    })
 }
